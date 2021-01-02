84f5c7e44876ff5c4eb3063abf7d9bb5b5828041
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2021-01-02 09:38:03 +0100

    docker: mount the needed volumes

--- core/docker.mk
+++ core/docker.mk
@@ -38,11 +38,25 @@ DOCKER_RUN += -v /etc/group:/etc/group:ro
 DOCKER_RUN += -w ${REPODIR}
 DOCKER_RUN += -v ${REPODIR}:${REPODIR}
 
+# Mount other needed volumes
+DOCKER_VOLUMES := ${BUILDDIR} ${DL_DIR} ${SSTATE_DIR}
+
+define mount-volume
+ ifeq ($(filter ${REPODIR}/%,$(abspath ${1})),)
+  DOCKER_RUN += -v ${1}:${1}
+ endif
+endef
+
+$(foreach volume,${DOCKER_VOLUMES},$(eval $(call mount-volume,${volume})))
+
 # Use the previously build image
 DOCKER_RUN += ${DOCKER_TAG}
 
 .PHONY: .docker-run
-.docker-run: .docker-build
+.docker-run: .docker-build | ${DOCKER_VOLUMES}
 	${DOCKER_RUN} ${MAKE_FORWARD} -f ${WZDIR}/core/config.mk
+
+${DOCKER_VOLUMES}:
+	mkdir -p $@
 
 .forward: .docker-run
