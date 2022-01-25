0df0aa6b5a4370b87cb39de065cb88d3985a3f1b
Jean-Marie Lemetayer <jeanmarie.lemetayer@gmail.com>
2022-01-25 15:17:36 +0100

    docker: oe-init-build-env: improve variable export from env

--- core/oe-init-build-env.mk
+++ core/oe-init-build-env.mk
@@ -7,7 +7,7 @@ include ${WZDIR}/core/lib/forward.mk
 
 OE_INIT_BUILD_ENV ?= ${REPODIR}/platform/poky/oe-init-build-env
 
-BB_EXPORT_VARIABLES := REPODIR BUILDDIR DL_DIR SSTATE_DIR DISTRO MACHINE
+override BB_EXPORT_VARIABLES += ${DEFAULT_BB_EXPORT_VARIABLES}
 
 define export-variable
  ifdef ${1}
