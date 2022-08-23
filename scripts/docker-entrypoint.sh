945bc78a380d7bc7ab2929d3836d07482385c0d7
Jean-Marie Lemetayer <j.lemetayer@kerlink.fr>
2022-08-23 15:45:44 +0200

    docker: entrypoint: do not create home directory
    The home directory already exist in the container due to the
    ssh mount.
    
    Force useradd not to create the home directory to avoid warnings.

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -13,10 +13,14 @@ groupadd \
 	docker
 
 useradd \
+	--home-dir /home/docker --no-create-home \
 	${OB_DOCKER_UID:+--non-unique --uid ${OB_DOCKER_UID}} \
 	--gid docker \
 	${OB_DOCKER_GROUPS:+--groups ${OB_DOCKER_GROUPS}} \
 	docker
+
+# Adjust rights for the user home
+chown -R docker:docker /home/docker
 
 # Execute the command as docker
 COMMAND=$(which ${1})
